a23d20f9a288ec1ef7ab481901ddfdbf4f4093bd
Igwe Acha Akwu <successoracha1995@gmail.com>
2023-01-03 11:35:56 +0100

    Defined all four routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,30 @@
 import { FourOhFour } from 'pages/404'
 import { Suspense, lazy } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
+import PrivateRoute from 'routes/ProtectedRoute'
+import { ROUTES } from 'utils/constants'
 
 const Home = lazy(() => import('pages/Home'))
+const LoginPage = lazy(() => import('pages/Login'))
+const DashboardPage = lazy(() => import('pages/Dashboard'))
+const UsersPage = lazy(() => import('pages/Users'))
+const UserInfoPage = lazy(() => import('pages/User'))
 
 const App = () => {
     return (
         <Suspense fallback='loading...'>
-            <Routes>
-                <Route path='/' element={<Home />} />
-                <Route path='*' element={<FourOhFour />} />
-            </Routes>
+            <Switch>
+                <Route exact path={ROUTES.home} component={Home} />
+                <Route exact path={ROUTES.login} component={LoginPage} />
+
+                <PrivateRoute
+                    path={ROUTES.dashboard}
+                    component={DashboardPage}
+                />
+                <Route exact path={ROUTES.users} component={UsersPage} />
+                <Route exact path={ROUTES.user} component={UserInfoPage} />
+                <Route exact path={ROUTES.notFound} component={FourOhFour} />
+            </Switch>
         </Suspense>
     )
 }
---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+    // private
+    dashboard: '/dashboard',
+    users: '/users',
+    user: '/users/:id',
+    // public
+    login: '/login',
+    home: '/',
+    notFound: '*'
+}
